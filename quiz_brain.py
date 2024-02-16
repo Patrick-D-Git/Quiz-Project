660176e3131367926916b8500f18e16bf57a17c4
Patrick D <patrick.montejo@gmail.com>
2024-02-15 22:01:28 -0500

    added docstring in quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,15 +6,18 @@ class Quizbrain:
         self.score = 0
 
     def still_has_questions(self):
+        """Checks if there are still questions in the list."""
         return self.question_number < len(self.question_list)
 
     def next_question(self):
+        """Gets the next question from the question list"""
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text}. (True/False)?: ")
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer, current_answer):
+        """check the user's answer and adds score if correct."""
         if user_answer.lower() == current_answer.lower():
             self.score += 1
             print("You got it right!")
